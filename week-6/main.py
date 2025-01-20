cba5bdd3eecf0bc14ca1f1ee8fcb3f68ef998057
khkesav <mindless3wizards@gmail.com>
2025-01-21 11:08:39 +1300

    Update Column First

--- week-6/main.py
+++ week-6/main.py
@@ -15,3 +15,15 @@ data = [
 ]
 extracted_date = [item for item in data if item["age"] < 25]
 print(extracted_date)
+
+# use list comprehension to flatten the matrix
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+flattened_matrix = [i for row in matrix for i in row]
+print(flattened_matrix)
+
+# use list comprehension to flatten the matrix and print first column only
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+column_matrix = [num for row in matrix for num in row]
+for i in range(0, len(column_matrix), 3):
+    print(column_matrix[i])
+# print(column_matrix)
