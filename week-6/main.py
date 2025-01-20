1c86199820a79d0712e43e2c700c31a84278b514
khkesav <mindless3wizards@gmail.com>
2025-01-21 11:35:44 +1300

    Update grades with enumerate

--- week-6/main.py
+++ week-6/main.py
@@ -22,8 +22,9 @@ flattened_matrix = [i for row in matrix for i in row]
 print(flattened_matrix)
 
 # use list comprehension to flatten the matrix and print first column only
+size = 3
 matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 column_matrix = [num for row in matrix for num in row]
-for i in range(0, len(column_matrix), 3):
+for i in range(0, len(column_matrix), size):
     print(column_matrix[i])
-# print(column_matrix)
+
