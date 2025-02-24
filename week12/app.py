eb48df343f24456c0dbbe298d00f4d05a617e300
khkesav <mindless3wizards@gmail.com>
2025-02-25 12:05:58 +1300

    Update application with common template

--- week12/app.py
+++ week12/app.py
@@ -1,3 +1,4 @@
+from flask import request
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -14,9 +15,9 @@ def contact():
 def about():
     return render_template('about.html')
 
-@app.route('/profile/<name>')
-def user(name):
-    return render_template('profile.html', username=name)
+@app.route('/profile/<name>/<int:user_id>')
+def user(name, user_id):
+    return render_template('profile.html', username=name, id=user_id)
 
 if __name__ == '__main__':
     app.run(debug=True)
