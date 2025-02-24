a955bf59c8d6fbcec496cb494b6bb60f8e759998
khkesav <mindless3wizards@gmail.com>
2025-02-24 15:36:56 +1300

    Contact page

--- week12/app.py
+++ week12/app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 @app.route('/about')
 def about():
     return render_template('about.html')
