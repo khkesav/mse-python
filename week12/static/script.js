62cd9695338699ddba6c42efe88b1916761bebe0
khkesav <mindless3wizards@gmail.com>
2025-02-24 16:01:04 +1300

    Update image preview

--- week12/static/script.js
+++ week12/static/script.js
@@ -5,4 +5,18 @@ document.addEventListener('DOMContentLoaded', function() {
             alert(alert_message);
        });
     });
+
+    const previewImage = document.querySelector('.block-image__preview');
+    document.getElementById('fileInput').addEventListener('change', function(event) {
+        const file = event.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = function(e) {
+                const img = document.createElement('img');
+                img.src = e.target.result;
+                previewImage.appendChild(img);
+            };
+            reader.readAsDataURL(file);
+        }
+    });
 })
